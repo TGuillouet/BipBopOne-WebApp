b51ee3a93a706a3bfd7e22b273aae3c71dffb780
Grandpierre Adrien <adrigrand@yahoo.fr>
2020-01-16 16:52:03 +0100

    send fichier en fonction du mod choice

--- src/components/home.js
+++ src/components/home.js
@@ -1,64 +1,103 @@
 import React from 'react';
-import {storage} from '../firebase'
-import {NotificationContainer, NotificationManager} from 'react-notifications';
+import { storage } from '../firebase'
+import { NotificationContainer, NotificationManager } from 'react-notifications';
 import 'react-notifications/lib/notifications.css';
 
-export default class Home extends React.Component{
+const idU = 'qH1m2ckbI4QUtpyTAoDsVr7v5qZ2';
+
+export default class Home extends React.Component {
 
     constructor(props) {
         super(props);
         this.state = {
-          image: null,
-          url: '',
-          progress: 0
+            dropSelectedView: null,
+            upladedFiles: {},
         }
     }
 
     handleChange = e => {
         if (e.target.files[0]) {
-          const image = e.target.files[0];
-          this.setState(() => ({image}));
+            const upladedFile = e.target.files[0];
+            const name = e.target.name
+            this.setState(
+                (prev) => ({ 
+                    upladedFiles: { 
+                        ...prev.upladedFiles, 
+                        [name]: upladedFile 
+                    } 
+                })
+            )
         }
-      }
-      handleUpload = () => {
-          const {image} = this.state;
-          const uploadTask = storage.ref(`images/${image.name}`).put(image);
-          uploadTask.on('state_changed', 
-          (snapshot) => {
-            // progrss function ....
-            const progress = Math.round((snapshot.bytesTransferred / snapshot.totalBytes) * 100);
-            this.setState({progress});
-          }, 
-          (error) => {
-               // error function ....
-            console.log(error);
-          }, 
-        () => {
-            // complete function ....
-            storage.ref('images').child(image.name).getDownloadURL().then(url => {
-                console.log(url);
-                this.setState({progress : 0});
-                NotificationManager.success('Success message', 'Send success');
-                
-            })
-        });
-      }
-  render(){
-    const style = {
-        height: '100vh',
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center',
-        justifyContent: 'center'
-      };
-      return(
-        <div style={style}>
-        <span>Hello</span>
-        <progress value={this.state.progress} max="100"/>
-        <input type="file" onChange={this.handleChange}/>
-        <button onClick={this.handleUpload}>Upload</button>
-        <NotificationContainer/>
-        </div>
-      )
-  }
+    }
+
+    handleUpload = () => {
+        const { upladedFiles } = this.state;
+        Object.values(upladedFiles).map( file =>  {
+            console.log(file)
+            const uploadTask = storage.ref(`test/${file.name}`).put(file);
+            uploadTask.on(storage.TaskEvent.STATE_CHANGED,
+                () => {
+                    // progrss function ....
+                },
+                (error) => {
+                    // error function ....
+                    console.log(error);
+                },
+                () => {
+                    // complete function ....
+                       NotificationManager.success('Success message', 'Send success');
+                });
+        })
+    }
+
+    handleDropSelectedChange = e => {
+        this.setState({dropSelectedView : e.target.value})
+    }
+
+    dropView() {
+        if (this.state.dropSelectedView == "wavefront") {
+            return (
+                <div>
+                    <span>wavefront</span>
+                    <input name="model" type="file" onChange={this.handleChange} accept=".png,.obj" />
+                    <input name="material" type="file" onChange={this.handleChange} accept=".png,.mtl" />
+                    <button onClick={this.handleUpload}>Upload</button>
+                </div>
+            )
+        }
+        else if (this.state.dropSelectedView == "gltf") {
+            return (
+                <div>
+                    <span>gltf</span>
+                    <input type="file" onChange={this.handleChange} accept=".png,.gltf" />
+                    <button onClick={this.handleUpload}>Upload</button>
+                </div>
+            )
+        }
+        else {
+            return
+        }
+    }
+
+    render() {
+        const style = {
+            height: '100vh',
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+            justifyContent: 'center'
+        };
+        return (
+            <div style={style}>
+                <span>Hello</span>
+                <select onChange={this.handleDropSelectedChange}>
+                    <option value="">--Select export--</option>
+                    <option value="wavefront">WaveFront</option>
+                    <option value="gltf">GLTF</option>
+                </select>
+                {this.dropView()}
+                <NotificationContainer />
+            </div>
+        )
+    }
 }
