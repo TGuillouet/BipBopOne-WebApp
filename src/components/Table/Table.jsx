b092e774826481e57b424f066ade7ddca4665542
Thomas Guillouët <thomas.guillouet@outlook.com>
2020-03-26 17:07:26 +0100

    Ajout du spinner

--- src/components/Table/Table.jsx
+++ src/components/Table/Table.jsx
@@ -1,21 +1,34 @@
 import React from "react";
 import Proptypes from "prop-types";
 
+import Loader from 'react-loader-spinner'
+
 function Table(props) {
     const rows = props.items.map(props.render)
 
-    return (
+    return ((props.isLoading)? <MyLoader />: (
         <table className='table is-striped is-hoverable'>
             <tbody>
-            {rows}
+                {rows}
             </tbody>
         </table>
-    )
+    ));
+}
+
+function MyLoader() {
+    return (
+        <div style={{display: "flex", alignItems: "center", justifyContent: "center"}}>
+            <div style={{ margin: "auto" }}>
+                <Loader type="ThreeDots" color="#000000" height={80} width={80} />
+            </div>
+        </div>
+    );
 }
 
 Table.propTypes = {
     items: Proptypes.array.isRequired,
-    render: Proptypes.func.isRequired
+    render: Proptypes.func.isRequired,
+    isLoading: Proptypes.bool
 };
 
 export default Table;
