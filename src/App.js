edd52b3c97b28e6ea2a25e98a7b72f02b3066c59
Unknown <adrigrand@yahoo.fr>
2020-03-26 11:09:18 +0100

    j'avance comme je peux ;-;

--- src/App.js
+++ src/App.js
@@ -2,9 +2,22 @@ import React from 'react';
 import './App.css';
 import Home from './components/home'
 
+import ProjectView from './components/ProjectView'
+import MenuView from './components/MenuView';
+
+import 'bulma/css/bulma.css'
+
 function App() {
+  document.body.style.overflow = "hidden"
   return (
-    <Home/>
+    <div class="columns">
+      <div class="column is-1">
+        <MenuView />
+      </div>
+      <div class="column">
+        <ProjectView />
+      </div>
+    </div>
   );
 }
 
