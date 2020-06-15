49788c8c5abec081c1f65d5c186804be88a372d7
Thomas Guillouët <thomas.guillouet@outlook.com>
2020-06-15 14:10:45 +0200

    wip test
    Erreur [ReferenceError: MutationObserver is not defined]

---
+++ src/components/ContactModal/ContactForm.spec.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+import {render} from "@testing-library/react";
+import '@testing-library/jest-dom/extend-expect'
+
+import ContactForm from "./ContactForm";
+
+
+describe("ContactForm", () => {
+  it("should render correctly", () => {
+    const { container } = render(
+      <ContactForm />
+    );
+
+    expect(container.firstChild).not.toBeNull()
+  })
+});
