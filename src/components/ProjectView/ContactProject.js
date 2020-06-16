e53c291b93e2963c4056c5820e27be29333a018c
Unknown <adrigrand@yahoo.fr>
2020-06-16 14:37:38 +0200

    #53 Ajouter un contact partagé à la liste de contacts
    ajouter au contact de l'utilisateur une personne de la whitelist

--- src/components/ProjectView/ContactProject.js
+++ src/components/ProjectView/ContactProject.js
@@ -1,23 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 import { useForm } from "react-hook-form";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faUserPlus } from "@fortawesome/free-solid-svg-icons";
 import { Table } from "../Table";
 
 import { updateProjectDetail } from "../../services/projects/projectsSevice";
+import { upsertContact } from "../../services/contacts/contacts";
+import Modal from "../Modal/Modal";
+import { ContactForm } from "../ContactForm";
 
 function ContactProject(props) {
 
     const { handleSubmit, register, errors, reset } = useForm();
+
+    const [createModalDisplayed, setCreateModalDisplayed] = React.useState(false);
 
     const generateRowComponentContact = (item) => {
         return (
             <tr>
                 <td style={{ verticalAlign: "middle" }}>{item}</td>
                 <td><button class="button">Bloquer</button></td>
-                <td><button class="button">Ajouter contact</button></td>
+                <td>
+                    <button class="button is-primary" onClick={() => setCreateModalDisplayed(true)}>
+                        <FontAwesomeIcon icon={faUserPlus} />
+                    </button>
+                </td>
+                <Modal isActive={createModalDisplayed} onClose={toggleCloseModal}>
+                    <ContactForm data={{ email: item, first_name: "", last_name: "" }} onSubmit={saveContact} />
+                </Modal>
             </tr>
         );
+    };
+
+    const saveContact = async (contactData) => {
+        try {
+            await upsertContact(props.userId, contactData.id, contactData)
+            setCreateModalDisplayed(false);
+        } catch (error) {
+            console.error(error);
+        }
     };
 
     const onSubmit = async value => {
@@ -25,6 +46,13 @@ function ContactProject(props) {
         await updateProjectDetail(props.userId, props.projectId, { whitelist: props.whitelist })
         reset({ values: { email: "" } });
     }
+
+
+    const toggleCloseModal = React.useCallback(() => {
+        setCreateModalDisplayed(!createModalDisplayed);
+    }, [createModalDisplayed]);
+
+
     return (
         <div>
             <form onSubmit={handleSubmit(onSubmit)}>
@@ -39,7 +67,7 @@ function ContactProject(props) {
                     })}
                 />
                 {errors.email && errors.email.message}
-                <button class="button is-primary" type="submit"><FontAwesomeIcon icon={faUserPlus} /></button>
+                <button class="button is-primary" type="submit">+ Add</button>
             </form>
             <Table
                 items={props.whitelist}
